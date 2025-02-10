4333aff236396c9a49f543b8faf842be4c8cc77e
Jeff Glaum <jeffglaum@live.com>
2025-02-10 11:25:34 -0800

    fix intro message text to read better

--- src/components/intro_message.rs
+++ src/components/intro_message.rs
@@ -6,33 +6,45 @@ pub fn IntroMessage() -> impl IntoView {
         <div class="text-base px-28">
             <p class="pb-4"><span class="font-bold text-xl whitespace-normal">Introducing the Open Device Partnership (ODP)</span></p>
 
-            <p class="pb-4 text-lg">ODP is an initiative designed to transform device fundamentals in the Windows ecosystem. By providing 
-            high quality and easy to reuse components, ODP strives to set new standards for security, performance, battery life, and reliability.</p>
+            <p class="pb-4 text-lg">ODP is an open initiative aimed at making it easier to build secure Windows devices with solid 
+            fundamentals.  Integral to this vision is the idea of leveraging industry standards to build system software that spans 
+            the full range of silicon options available and to simplify and accelerate development of high-quality devices.  ODP strives 
+            to set new standards for security, performance, battery life, and reliability while maximizing code re-use.</p>
 
             <p class="pb-4"><span class="font-bold text-xl whitespace-normal">ODP Projects</span></p>
-            <ul class="pb-4 text-lg">
-                <li class="list-disc list-inside"><i>Fast and Minimal Boot Firmware</i> - a secure and efficient boot firmware for Windows devices</li>
-                <li class="list-disc list-inside"><i>Hardened Embedded Controller (EC)</i> - standardizes and secures the embedded controller firmware across the Windows ecosystem</li>
-                <li class="list-disc list-inside"><i>Modern Power & Thermal Framework (MPTF)</i> - a silicon agnostic performance and thermal management framework to deliver superior battery life</li>
+            <ul class="pb-4 text-lg list-disc">
+                <li class="list-inside"><i>Fast and Minimal Boot Firmware</i> - A secure and efficient boot firmware for Windows devices</li>
+                <li class="list-inside"><i>Hardened Embedded Controller Firmware</i> - An extensible, MCU-agnostic, secure embedded controller firmware</li>
+                <li class="list-inside"><i>Standardized Embedded Controller Services</i> - A common method for interfacing embedded controller services with Windows</li>
             </ul>
-            <p class="pb-4"><span class="text-lg">We are also interested in adding new projects to the partnership that align with our shared goals (consult our governance documentation for more information).</span></p>
+            <p class="pb-4"><span class="text-lg">While we have ambitious goals for these first projects, we are also interested in 
+            adding new projects to the partnership that align with our shared goals.  More information about partnership governance 
+            and how to start new projects will be provided shortly.  In the meantime, please reach out to <a href="mailto:odpadmin@microsoft.com" style="text-decoration:underline;">ODP Administrators</a>  
+            with questions or comments.</span></p>
 
             <p class="pb-4"><span class="font-bold text-xl whitespace-normal">Value Proposition</span></p>
             <ul class="pb-4 text-lg">
-                <li class="list-disc list-inside"><i>Enhanced Security</i> - secures the boot process and hardened EC firmware, protecting devices from vulnerabilities</li>
-                <li class="list-disc list-inside"><i>Improved Performance</i> - optimizing performance and thermal management, MPTF addresses inconsistent battery life</li>
-                <li class="list-disc list-inside"><i>Accelerated Development</i> - open-source collaboration with partners, enabling faster and more efficient product development</li>
-                <li class="list-disc list-inside"><i>Standardized Solutions</i> - designed to work across different architectures (x86 & Arm) and operating systems</li>
+                <li class="list-disc list-inside"><i>Enhanced Security</i> - As security threats continue to evolve, it is critical we take 
+                    bold steps to protect devices from vulnerabilities by reducing the attack surface area, using secure hardware features, 
+                    using modern programming languages to reduce human error-induced problems, and generally thinking about security first 
+                    in every piece of code we design and author.</li>
+                <li class="list-disc list-inside"><i>Standardization</i> - While it is critical for device partners to differentiate in features and 
+                    capabilities, unfortunately a large fraction of device software is often simply the infrastructure and plumbing 
+                    necessary to pull everything together.  Developing and maintaining this software is a development tax to be paid 
+                    and worse, often paid for each device and each architecture (x86 & ARM).  Developing and building on top of industry 
+                    standards offers one option to simplify and maximize re-use and it is a centerpiece of the ODP strategy.</li>
+                <li class="list-disc list-inside"><i>Accelerated Development</i> - ODP is built on open-source collaboration with partners, 
+                    sharing common goals and solutions, thus enabling faster and more efficient product development.</li>
             </ul> 
 
             <p class="pb-4"><span class="font-bold text-xl whitespace-normal">Partner-Oriented Vision</span></p>
-            <p class="pb-4 text-lg">ODP is built on a foundation of collaboration and innovation. We are committed to creating an ecosystem of partners, 
-            including OEMs, silicon providers, and independent developers, to build secure and high-quality devices.</p>
+            <p class="pb-4 text-lg">We are committed to creating an inclusive ecosystem of partners, including device OEM/ODMs, 
+            IHVs, Silicon Vendors, independent developers, security researchers, and anyone interested in helping to build secure 
+            and high-quality devices.</p>
 
             <p class="pb-4"><span class="font-bold text-xl whitespace-normal">Get Involved</span></p>
-            <p class="pb-4 text-lg">Our goal is to create a foundation that not only meets but exceeds current state of the art, driving innovation and 
-            raising the bar for Windows devices. We are also open to collaborating with partners building other categories of devices. For more information 
-            about the ODP project and partnership opportunities, please consult the documentation and issue your first pull request.</p>
+            <p class="pb-4 text-lg">For more information about ODP and partnership opportunities, please consult the documentation, 
+            clone source code from our growing list of public repositories and issue your first pull request!</p>
         </div>
     }
 }
