651fce02ffa1bc27190d5d7808f23360d015826c
Jeff Glaum <jeffglaum@live.com>
2025-07-02 16:38:52 -0700

    add back button to ec team page

--- src/pages/team_ec.rs
+++ src/pages/team_ec.rs
@@ -28,6 +28,55 @@ pub fn TeamEC() -> impl IntoView {
         
             <div class="w-full min-h-screen overflow-x-hidden">
                 <Header />
+                <a
+                    href="javascript:history.back()"
+                    class="block"
+                    style="margin: 0; padding: 0;"
+                >
+                    <picture>
+                        <source srcset="/images/dark/backbutton.svg" media="(prefers-color-scheme: dark)" />
+                        <img
+                            src="/images/light/backbutton.svg"
+                            alt="Back"
+                            style="margin: 0; padding: 0; display: block;"
+                        />
+                    </picture>
+                </a>
+                <section
+                    class="background_primary"
+                    style="
+                        padding: 120px;
+                        margin-top: -80px;
+                    "
+                >
+                    <div class="flex flex-row gap-[80px]">
+                        {/* Left Column */}
+                        <div class="flex flex-col items-start" style="width: 700px;">
+                            <span
+                                class="h1"
+                                style="
+                                    display: block;
+                                    text-align: left;
+                                "
+                            >
+                                {"Meet the team"}
+                            </span>
+                        </div>
+                        {/* Right Column */}
+                        <div class="flex flex-col items-start" style="width: 600px;">
+                            {/* Team Introduction */}
+                            <span
+                                class="mono"
+                                style="
+                                    display: block;
+                                    text-align: left;
+                                "
+                            >
+                                {"The Secure EC team ..."}
+                            </span>
+                        </div>
+                    </div>
+                </section>
                 <Footer />
             </div>
             
